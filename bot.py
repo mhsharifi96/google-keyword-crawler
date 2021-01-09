79f0d2d0abee552ac2fe38cdea9d3683b36f4bba
mhsharifi96 <mh.sh7676@gmail.com>
2021-01-09 14:39:35 +0330

    Update bot.py
    fix

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import  time
 # Remember to use your own values from my.telegram.org!
 api_id = 281931
 api_hash = '2536e06ebbf8c63edc74aa4a0f7c062e'
-bot_token = "970347099:AAG3f5Mgaa1QRbMxg-tNeKmFT1dnT3_Cm_o"
+bot_token = "1511898542:AAE2E_pJb63N4bH2AKdvFhZDClYDwtDkeCw"
 client =  TelegramClient('bot', api_id, api_hash)
 regex_lsi = r"key\s?:\s?"
 
